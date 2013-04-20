fc247ad5ca4e7b833628f9617ced24a478b1fcf0
Kyle Ellrott <kellrott@gmail.com>
2013-04-19 22:25:37 -0700

    Adding more faked data to the unit tests. Fixing some rules in the dogma

--- tests/test_pathway_infer.py
+++ tests/test_pathway_infer.py
@@ -10,12 +10,23 @@ from pathway_tools.infer import pathway_inference, InferenceConfig
 
 
 class TestCase(unittest.TestCase):    
-    def test_infer(self):
+    def test_infer_1(self):
 
         for s in ["sample_1","sample_2","sample_3","sample_4","sample_5","sample_6"]:
             args = InferenceConfig(dogma='active_only.yaml', 
-                pathway='toy_pathway.tab', 
-                evidence=[('active', 'toy_data.tab')],
-                sample=s
+                pathway='toy_pathway_1.tab', 
+                evidence=[('active', 'toy_data_1.tab')],
+                sample=[s]
+            )
+            pathway_inference(args)
+
+
+    def test_infer_2(self):
+
+        for s in ["sample_1","sample_2","sample_3","sample_4"]:
+            args = InferenceConfig(dogma='active_only.yaml', 
+                pathway='toy_pathway_2.tab', 
+                evidence=[('active', 'toy_data_2.tab')],
+                sample=[s]
             )
             pathway_inference(args)
