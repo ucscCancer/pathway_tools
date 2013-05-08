e5fb4fb85d0ef3ebc113375ed46225fae22e3e34
Kyle <kellrott@soe.ucsc.edu>
2013-05-08 12:02:16 -0700

    Adding more options to the pathway search tool

--- scripts/pathway_search.py
+++ scripts/pathway_search.py
@@ -7,11 +7,16 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--query-paradigm', default=None)
-    parser.add_argument('--query-xgmml', default=None)
-    parser.add_argument('--base-paradigm', default=None)
-    parser.add_argument('--base-xgmml', default=None)
+    parser.add_argument('--query-paradigm', help="Input query in Paradigm graph format", default=None)
+    parser.add_argument('--query-xgmml',  help="Input query in XGMML graph format", default=None)
+    parser.add_argument('--target-paradigm', help="Search Target in Paradigm graph format", default=None)
+    parser.add_argument('--target-xgmml', help="Search Target in XGMML graph format", default=None)
+    parser.add_argument('--query-pathattr', default="pid")
+    parser.add_argument('--target-pathattr', default="pid")
+    parser.add_argument("--query-nodetype", default=None)
+    parser.add_argument("--target-nodetype", default=None)
     parser.add_argument("--min", "-m", type=int, default=1)
+    parser.add_argument("--top", type=int, default=None)
 
     args = parser.parse_args()
 
@@ -24,28 +29,48 @@ if __name__ == "__main__":
         query_net = convert.read_xgmml(handle)
     
 
-    if args.base_paradigm:
-        handle = open(args.base_paradigm) 
-        base_net = convert.read_paradigm_graph(handle)
+    if args.target_paradigm:
+        handle = open(args.target_paradigm) 
+        target_net = convert.read_paradigm_graph(handle)
         
-    if args.base_xgmml:
-        handle = open(args.base_xgmml) 
-        base_net = convert.read_xgmml(handle)
+    if args.target_xgmml:
+        handle = open(args.target_xgmml) 
+        target_net = convert.read_xgmml(handle)
     
-    pathways = {}
-    for n in base_net.node:
-        for pid in base_net.node[n]['pid']:
-            pathways[pid] = True
+    target_pathways = {}
+    for n in target_net.node:
+        if args.target_nodetype is None or target_net.node[n]['type'] == args.target_nodetype:
+            for pid in target_net.node[n][args.target_pathattr]:
+                target_pathways[pid] = target_pathways.get(pid, 0) + 1
+
+    query_pathways = {}
+    for n in query_net.node:
+        if args.query_nodetype is None or query_net.node[n]['type'] == args.query_nodetype:
+            for pid in query_net.node[n][args.query_pathattr]:
+                query_pathways[pid] = query_pathways.get(pid, 0) + 1
+
     
     hits = {}
     for n in query_net.node:
-        if n in base_net.node:
-            for pid in base_net.node[n]['pid']:
-                if pid not in hits:
-                    hits[pid] = []
-                hits[pid].append(n)
+        if n in target_net.node:
+            for q_pid in query_net.node[n][args.query_pathattr]:
+                if q_pid not in hits:
+                    hits[q_pid] = {}
+                for t_pid in target_net.node[n][args.target_pathattr]:
+                    if t_pid not in hits[q_pid]:
+                        hits[q_pid][t_pid] = []
+                    hits[q_pid][t_pid].append(n)
 
-    k = hits.keys()
-    k.sort( key=lambda x:len(hits[x]), reverse=True )
-    for i in k[:10]:
-        print "%s = %s" % (i, ",".join(hits[i]))
+    for q in hits:
+        hit_list = hits[q].keys()
+        hit_list.sort(key=lambda x:len(hits[q][x]), reverse=True)
+        if args.top is not None:
+            hit_list = hit_list[:args.top]
+        for t in hit_list:
+            if len(hits[q][t]) >= args.min:
+                print "%s = %s : %d : ( %f %f ) : (%s)" % (
+                        q, t, len(hits[q][t]),
+                        float(len(hits[q][t])) / float(query_pathways[q]), 
+                        float(len(hits[q][t])) / float(target_pathways[t]), 
+                        ",".join(hits[q][t])
+                    )
