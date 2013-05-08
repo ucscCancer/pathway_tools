2d97b311045e2091ea2aaf451552276c7284cfc5
Kyle Ellrott <kellrott@gmail.com>
2013-05-08 09:41:18 -0700

    Adding some options to the biopax parser

--- scripts/read_biopax.py
+++ scripts/read_biopax.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python 
 
 import sys
+import argparse
 from pathway_tools.biopax import BioPax
 from pathway_tools import convert
 
 if __name__ == "__main__":
-	b = BioPax()
-	b.load(sys.argv[1])
-	
-	for gr in b.toNet():
-		#convert.write_paradigm_graph(gr, sys.stdout)
-		convert.write_xgmml(gr, sys.stdout)
-		
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', "--output", default=None)
+    parser.add_argument("input")
+    args = parser.parse_args()
+
+    b = BioPax()
+    b.load(args.input)
+    
+    for gr in b.toNet():
+        #convert.write_paradigm_graph(gr, sys.stdout)
+        if args.output:
+            handle = open(args.output, "w")
+            convert.write_xgmml(gr, handle)
+        else:
+            convert.write_xgmml(gr, sys.stdout)
