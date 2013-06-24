b18d907623393e2afe2e4437a84dea563faccdd9
Kyle <kellrott@soe.ucsc.edu>
2013-06-24 16:39:58 -0700

    Changes to script used to build repo from original superpathway file

--- scripts/build_pathway_repo.py
+++ scripts/build_pathway_repo.py
@@ -3,50 +3,39 @@
 
 import sys
 import os
-import network_convert
+from pathway_tools import convert
 import yaml
 
 if __name__ == "__main__":
 	handle = open(sys.argv[1])
-	gr = network_convert.read_xgmml(handle)
+	gr = convert.read_paradigm_graph(handle, strict=False)
 	handle.close()
 
 	name_tab = {}
 	handle = open(sys.argv[2])
 	for line in handle:
 		tmp = line.rstrip().split("\t")
-		name_tab[tmp[1]] = [tmp[0], tmp[2]]
+		name_tab[tmp[0]] = tmp[1:]
 	outdir = sys.argv[3]
 	
-	pathways = {}
-	for n in gr.node:
-		if 'pathway' in gr.node[n]:
-			for path in gr.node[n]['pathway']:
-				pathways[path] = True
-	
 	i = 1
-	for path in pathways:
-		if path in name_tab:
-			pathid = "PID%s" % (name_tab[path][0])
-		else:
-			pathid = "TMP%d" % (i)
-			i += 1
+	for pathid in name_tab:
 		print pathid
 		pathdir = os.path.join(outdir, pathid)
 		if not os.path.exists(pathdir):
 			os.mkdir(pathdir)
-		config = {"DESC" : str(path)}
-		if path in name_tab:
-			config["PID"] = int(name_tab[path][0])
-			config["SOURCE"] = str(name_tab[path][1])
+		config = {"DESC" : str(pathid)}
+		#if path in name_tab:
+		#	config["PID"] = int(name_tab[path][0])
+		#	config["SOURCE"] = str(name_tab[path][1])
 		
 		handle = open(os.path.join(pathdir, "INFO"), "w")
-		print path
+		print pathid
 		handle.write(yaml.dump(config, default_flow_style=False))
 		handle.close()
 		nodes = {}
 		for n in gr.node:
-			if 'pathway' in gr.node[n] and path in gr.node[n]['pathway']:
+			if n in name_tab[pathid]:
 				nodes[n] = True
 		handle = open(os.path.join(pathdir, "graph"), "w")
 		for n in nodes:
