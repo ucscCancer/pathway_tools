07ab84cea62d82a6f5829bd262d1fd15dea8f5ff
Kyle Ellrott <kellrott@gmail.com>
2013-11-13 23:02:25 -0800

    Adding scala code for extracting large Biopax files into an set of independent pathway files

---
+++ project/plugins.sbt
@@ -0,0 +1,6 @@
+resolvers += "Spray Repository" at "http://repo.spray.cc/"
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.10.1")
+//resolvers += "sbt-assembly-resolver-0" at "http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"
---
+++ src/main/scala/edu/ucsc/pathway/BioPaxExtract.scala
@@ -0,0 +1,155 @@
+
+package edu.ucsc.pathway
+
+import org.openrdf.rio.{RDFHandler, ParserConfig, RDFParser}
+import java.util.zip.GZIPInputStream
+import java.io.{FileOutputStream, FileInputStream, File}
+import org.openrdf.rio.rdfxml.{RDFXMLWriter, RDFXMLParser}
+import org.openrdf.model.{Statement,Resource}
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashSet
+import scala.collection.mutable
+
+
+class BioPax_Pathways() extends RDFHandler {
+  var pathway_list = new ArrayBuffer[Resource]()
+  val RDF_TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+  val BIOPAX_PATHWAY = "http://www.biopax.org/release/biopax-level3.owl#Pathway"
+
+  def startRDF() = {}
+  def endRDF() = {}
+  def handleComment(comment:String) = {}
+  def handleNamespace(prefix:String, uri:String) = {}
+
+  def handleStatement(st:Statement) = {
+    val b = st.getSubject();
+    if (st.getPredicate().toString == RDF_TYPE && st.getObject().toString == BIOPAX_PATHWAY) {
+      pathway_list += b;
+    }
+  }
+}
+
+class NetworkGrouper(seeds : Array[Resource]) extends RDFHandler {
+  val original_set = new HashSet[Resource]() ++ seeds;
+  var member_map = new HashMap[Resource,HashSet[Int]]();
+  var added = false;
+  var name_map = new HashMap[String,Int]();
+
+  seeds.zipWithIndex.foreach( x => {
+    member_map(x._1) = member_map.getOrElse(x._1, new HashSet[Int]()) ++ List(x._2);
+    name_map(x._1.toString().split("/").last) = x._2
+  } )
+
+
+  def startRDF() = {
+    added = false
+  }
+  def endRDF() = {}
+  def handleComment(comment:String) = {}
+  def handleNamespace(prefix:String, uri:String) = {}
+
+  def handleStatement(st:Statement) = {
+    val obj = st.getObject();
+    if (obj.isInstanceOf[Resource]) {
+      val subj = st.getSubject();
+      if (!original_set.contains(obj.asInstanceOf[Resource])) {
+        if (member_map.contains(subj)) {
+          if (!member_map.contains(obj.asInstanceOf[Resource])) {
+            member_map(obj.asInstanceOf[Resource]) = new HashSet[Int]() ++ member_map(subj);
+          } else {
+            val o_set = member_map(obj.asInstanceOf[Resource]);
+            val s_set = member_map(subj);
+            if (o_set.intersect(s_set) != s_set) {
+              o_set ++= s_set;
+              added = true;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+
+class BioPax_Splitter(val groups:Map[String,Int], val elementMap:Map[Resource, HashSet[Int]], val outdir:File) extends RDFHandler {
+  var pathway_list = new ArrayBuffer[Resource]()
+  val RDF_TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+  val BIOPAX_PATHWAY = "http://www.biopax.org/release/biopax-level3.owl#Pathway"
+
+  val output_map = new HashMap[Int,FileOutputStream]();
+  val writer_map = new HashMap[Int,RDFXMLWriter]();
+
+
+  def startRDF() = {
+    groups.foreach( x => {
+      val f = new FileOutputStream( new File(outdir, x._1 + ".owl") );
+      output_map(x._2) = f;
+      val w = new RDFXMLWriter(f);
+      w.startRDF();
+      writer_map(x._2) = w;
+    })
+  }
+  def endRDF() = {
+    groups.foreach( e => {
+      writer_map(e._2).endRDF();
+      output_map(e._2).close();
+    })
+  }
+  def handleComment(comment:String) = {}
+  def handleNamespace(prefix:String, uri:String) = {}
+
+  def handleStatement(st:Statement) = {
+    val b = st.getSubject();
+    val b_sets = elementMap.get(b);
+    if (b_sets.isDefined) {
+      for ( group <- b_sets.get) {
+        if (b_sets.get.contains(group)) {
+          writer_map(group).handleStatement(st);
+        }
+      }
+    }
+  }
+}
+
+
+
+object BioPaxExtract {
+
+  def parseFile(file_path:String, handler:RDFHandler) = {
+    val gzip = file_path.endsWith(".gz");
+
+    val fis = if (gzip)
+      new GZIPInputStream(new FileInputStream(file_path))
+    else
+      new FileInputStream(file_path);
+    val parser = new RDFXMLParser();
+    parser.setParserConfig( new ParserConfig(true, false, false,  RDFParser.DatatypeHandling.VERIFY) );
+    val baseURL = "http://pathwaycommons.org/"
+    parser.setRDFHandler(handler)
+    parser.parse(fis , baseURL)
+    fis.close();
+  }
+
+  def main(args: Array[String]) = {
+    val file_path = args(0);
+    val out_dir = args(1);
+    val pathway_scan = new BioPax_Pathways();
+    parseFile(file_path, pathway_scan);
+    println(pathway_scan.pathway_list.length)
+
+    val ng = new NetworkGrouper(pathway_scan.pathway_list.toArray)
+
+    do {
+      parseFile(file_path, ng);
+      println("Selection Cycle: " + ng.member_map.size + " elements");
+    } while (ng.added);
+
+    val writer = new BioPax_Splitter(ng.name_map.toMap, ng.member_map.toMap, new File(out_dir));
+    parseFile(file_path, writer);
+  }
+
+
+}
