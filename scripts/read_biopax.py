c2ffef5c2480bff09113639a8ebf8a38a65c3b00
Kyle Ellrott <kellrott@gmail.com>
2013-11-21 13:03:00 -0800

    Adding parallel processing to batch file conversion

--- scripts/read_biopax.py
+++ scripts/read_biopax.py
@@ -8,21 +8,70 @@ import networkx
 from pathway_tools.biopax import BioPaxFile, BioPaxSparql
 from pathway_tools import convert
 
+from multiprocessing import Pool
+
 re_clean = re.compile(r'[/\\ \n;]')
 re_namesplit = re.compile(r'[/ \#\&]')
 
+class ConvertTask:
+
+    def __init__(self, path_file, outdir, pathways=None):
+        self.path_file = path_file
+        self.pathways = pathways
+        self.outdir = outdir
+
+    def run(self):
+        path_file = self.path_file
+        if isinstance(path_file, basestring):
+            t = BioPaxFile()
+            t.load(path_file)
+            path_file = t
+        paths = []
+        for key,value in path_file.pathways().iteritems():
+            add = False
+            if self.pathways is None or value in self.pathways:
+                add = True
+            elif key in self.pathways:
+                add = True
+            else:
+                a_name = re_namesplit.split(key)[-1]
+                if a_name in self.pathways:
+                    add = True
+            if add:
+                paths.append(key)
+
+        for subnet in path_file.toNet(paths):                
+            gr = networkx.MultiDiGraph()
+            gr.graph['name'] = subnet.meta['name']
+            gr.graph['url'] = subnet.meta['url']
+            subnet.to_graph(gr)
+
+            name = re_namesplit.split(gr.graph['url'])[-1]
+            handle = open(os.path.join(self.outdir, name + ".xgmml"), "w")
+            
+            if args.paradigm:
+                convert.write_paradigm_graph(gr, handle)
+            else:
+                convert.write_xgmml(gr, handle)
+            
+            if handle != sys.stdout:
+                handle.close()
+
+def runner(x):
+    x.run()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', "--output", default=None)
     parser.add_argument('-p', '--pathways', action="append")
     parser.add_argument("-s", "--sparql", action="store_true", default=False)
     parser.add_argument("-l", "--list", action="store_true", default=False)
     parser.add_argument("-d", "--out-dir", default=None)
+    parser.add_argument("--cpus", type=int, default=1)
     
     parser.add_argument("--paradigm", action="store_true", default=False)
     
     
-    parser.add_argument("input")
+    parser.add_argument("input", nargs="+")
     args = parser.parse_args()
 
     file_list = []
@@ -30,54 +79,26 @@ if __name__ == "__main__":
     if args.sparql:
         file_list = [ BioPaxSparql(args.input) ]
     else:
-        b = BioPaxFile()
-        b.load(args.input)
-        file_list = [b]
+        file_list = args.input
 
     if args.list:
-        for f in file_list:
-            for a,b in f.pathways().iteritems():
+        for path_file in file_list:
+            if isinstance(path_file, basestring):
+                t = BioPaxFile()
+                t.load(path_file)
+                path_file = t
+            for a,b in path_file.pathways().iteritems():
                 print a, b.encode('ascii', errors='ignore')
     else:
-        
-        paths = []
-        for path_file in file_list:
-            for key,value in b.pathways().iteritems():
-                add = False
-                if args.pathways is None or value in args.pathways:
-                    add = True
-                elif key in args.pathways:
-                    add = True
-                else:
-                    a_name = re_namesplit.split(key)[-1]
-                    if a_name in args.pathways:
-                        add = True
-                if add:
-                    paths.append(key)
 
-            for subnet in b.toNet(paths):
-                
-                gr = networkx.MultiDiGraph()
-                gr.graph['name'] = subnet.meta['name']
-                gr.graph['url'] = subnet.meta['url']
-                subnet.to_graph(gr)
+        if args.cpus > 1:
+            runs = []
+            for path_file in file_list:
+                runs.append( ConvertTask(path_file, args.out_dir, args.pathways) )
+            p = Pool(args.cpus)
+            p.map(runner, runs)
+        else:
+            for path_file in file_list:
+                ConvertTask(path_file, args.out_dir, args.pathways).run()
 
-                name = re_namesplit.split(gr.graph['url'])[-1]
-                if args.out_dir:
-                    handle = open(os.path.join(args.out_dir, name + ".xgmml"), "w")
-                else:
-                    if args.output:
-                        if "%s" not in args.output:
-                            raise Exception("Output path string not formatted correctly (use %s))")
-                        handle = open(args.output % (name), "w")
-                    else:
-                        handle = sys.stdout
 
-                
-                if args.paradigm:
-                    convert.write_paradigm_graph(gr, handle)
-                else:
-                    convert.write_xgmml(gr, handle)
-                
-                if handle != sys.stdout:
-                    handle.close()
