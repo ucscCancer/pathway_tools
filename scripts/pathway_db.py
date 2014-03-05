fd43eb2aa23b6b14259d941b28a2d3d568f03d04
Kyle Ellrott <kellrott@gmail.com>
2014-03-05 15:28:53 -0800

    Removing some additional characters for output pathways

--- scripts/pathway_db.py
+++ scripts/pathway_db.py
@@ -386,8 +386,7 @@ class GraphBuilder:
                         data['label'] = re.sub("&gamma;", "gamma", data['label'], flags=re.IGNORECASE)
 
                     if self.args.rename_char or self.args.all:
-                        data['label'] = re.sub( r'[\'\"\:\?\\\*]', "_", data['label'])
-                        data['label'] = re.sub( r'\+', "postitive", data['label'])
+                        data['label'] = re.sub( r'[\'\"\:\?\\\*= \,]', "_", data['label'])
 
 
                     if 'label' in data and data['label'] in self.merge_map:
