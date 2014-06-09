d37a4ec27519ce191d480bc8ef9c4246479b5b8f
Kyle Ellrott <kellrott@gmail.com>
2014-06-09 12:56:04 -0700

    Adding network coverage stats

--- scripts/network_stats.py
+++ scripts/network_stats.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     parser.add_argument('--spf', help="SimplePathwayFormat File")
     parser.add_argument('--xgmml', help="XGMML File")
     parser.add_argument('--top', help="Top Count", type=int, default=10)
+    parser.add_argument('--coverage', help="Coverage File", default=None)
 
 
     args = parser.parse_args()
@@ -26,17 +27,29 @@ if __name__ == "__main__":
         gr = convert.read_xgmml(handle)
         handle.close()
 
+    coverage_set = {}
+    if args.coverage is not None:
+        with open(args.coverage) as handle:
+            for line in handle:
+                coverage_set[line.rstrip()] = 0
+
+
 
     type_count = {}
     for node in gr.node:
         if 'type' in gr.node[node]:
             node_type = gr.node[node]['type']
             type_count[node_type] = type_count.get(node_type, 0) + 1
+            if node in coverage_set:
+                coverage_set[node] += 1
 
 
     print("Node Count: %d" % (len(gr.nodes())))
     print("Edge Count: %d" % (len(gr.edges())))
     print("Connected Components: %d" % (networkx.number_connected_components(networkx.Graph(gr))))
+    if len(coverage_set):
+        hits = sum((1 for b in coverage_set if coverage_set[b] != 0))
+        print("Set Coverage: %d (%f)" % (hits, float(hits) / float(len(coverage_set))))
 
     cc = networkx.connected_components(networkx.Graph(gr))
     hist = {}
